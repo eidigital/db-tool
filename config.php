616a5be5742e39c9225b10329146d3f3bf79a958
Amir Andalibi <amir@accolade.us>
2017-10-03 11:58:44 -0400

    Config file

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$DEV_URL        = ''; // Your development URL
+
+$PRD_HOST     	= ''; // Your production server IP
+$PRD_DBNAME  	 	= ''; // Your production MySQL db name
+$PRD_USERNAME 	= ''; // Your production MySQL db user
+$PRD_PASSWORD 	= ''; // Your production MySQL db password
+
+$DEV_DBNAME     = ''; // Your Local MySQL db name
+$DEV_USERNAME   = ''; // Your Local MySQL db user
+$DEV_PASSWORD   = ''; // Your Local MySQL db password
